ba0220c98274af6f6ea758a7f7296bfca7b7272a
Lunarskii <lunarskii0@gmail.com>
2024-10-29 20:45:52 +0300

    environment variables are now read from the application session

--- src/settings.py
+++ src/settings.py
@@ -13,8 +13,6 @@ from pydantic_settings import (
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file=".env",
-        env_file_encoding="utf-8",
         extra="ignore",
     )
 
